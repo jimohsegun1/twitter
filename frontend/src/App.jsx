2b170821bb9295969cf8d0ddec04d61297861226
jimohsegun1 <jimohsegunj@gmail.com>
2024-05-08 17:10:42 +0100

    4. UI design with hard coded data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
-import NotificationPage from "./pages/notification/NotificationPage";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
        {/* A common component which will be visible in all pages */}
       <RightPanel />
